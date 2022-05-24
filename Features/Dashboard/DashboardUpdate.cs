829d6a7e181653850b7e3c3cc3c435be874c605a
shahinyusifli <sahinyusifli98@gmail.com>
2022-05-24 11:25:04 +0300

    closes #10 Add permision chack

--- Features/Dashboard/DashboardUpdate.cs
+++ Features/Dashboard/DashboardUpdate.cs
@@ -13,7 +13,7 @@ public class DashboardUpdate : ICarterModule
     {
        
 
-        app.MapPut("/dashboard/update/{topicId}",[Authorize] async (MessagesDb db, Topic updateTopic, int topicId) =>
+        app.MapPut("/dashboard/update/{topicId}",[Authorize(Roles = "admin")] async (MessagesDb db, Topic updateTopic, int topicId) =>
 {
     var topicItem = await db.Topics.FindAsync(topicId);
     var messageItem = await db.Messages.FirstOrDefaultAsync(x => x.TopicId == topicId);
