f41d569527e31d14509b718a6e9746db5aa9002c
shahinyusifli <sahinyusifli98@gmail.com>
2022-06-06 15:49:14 +0300

    closes #18

--- Features/Dashboard/DashboardPost.cs
+++ Features/Dashboard/DashboardPost.cs
@@ -4,11 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 public class DashboardPost : ICarterModule
 {
+
+      public class Message_from_frontend
+  {
+      public int MessageId { get; set; }
+      public string? messageName { get; set; }
+      public DateTime? Date { get; set;  }
+      public string? topicName { get; set; }
+      public string? userName { get; set; }
+    
+  }
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/dashbaord/post", async (Message message, MessagesDb db) =>
+        app.MapPost("/dashbaord/post", async (Message_from_frontend message, MessagesDb db) =>
         {
-            await db.Messages.AddAsync(message);
+            var db_message = new Message(); 
+
+           db_message.Date = DateTime.Now;
+           db_message.MessageContent = message.messageName;
+           db_message.TopicId = db.Topics.FirstOrDefault(x => x.TopicContent == message.topicName).TopicId;
+           db_message.UserId = db.Users.FirstOrDefault(x => x.UserName == message.userName).UserId;
+
+            
+            await db.Messages.AddAsync(db_message);
             await db.SaveChangesAsync();
             return Results.Created($"/topic/get/{message.MessageId}", message);
         });
